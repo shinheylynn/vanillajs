8cd067f12ed4db010fcb5c25ce778eb65e4b0701
shinheylynn <heylub@gmail.com>
2024-08-28 01:23:53 +0900

    wip

--- baseball.js
+++ baseball.js
@@ -1,1 +1,21 @@
-console.log("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!")
+function generateRandomNumber() {
+  const answer = [];
+
+  while (answer.length < 3) {
+    const number = Math.floor(Math.random() * 9) + 1;
+    answer.push(number);
+  }
+
+  return answer.join('');
+}
+
+function playGame() {
+  const answer = generateRandomNumber();
+  let attempts = 0;
+
+  console.log('숫자야구게임을 시작합니다.');
+  console.log(answer);
+  console.log(answer);
+}
+
+playGame();
